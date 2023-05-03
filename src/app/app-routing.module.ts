b6baa0956536c2c329007374247b264518e132a3
Seysoon <114059035+23jayson23@users.noreply.github.com>
2023-05-03 08:30:55 +0000

    sign up page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { SignInFacultyComponent } from './sign-in-faculty/sign-in-faculty.component';
-
+import { SignUpFacultyComponent } from './sign-up-faculty/sign-up-faculty.component';
 
 const routes: Routes = [
   {path: '', component: SignInFacultyComponent},
+  {path: 'sign-up', component: SignUpFacultyComponent},
 ];
 
 @NgModule({
