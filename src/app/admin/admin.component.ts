80f2fc58f8adc44fc7f69969593cc051e241fb19
Seysoon <114059035+23jayson23@users.noreply.github.com>
2023-05-04 04:31:20 +0000

    router

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -11,7 +11,16 @@ export class AdminComponent {
 
 constructor(public route:Router){}
 
-dashboard(){
-  this.route.navigate[("admin-page")]
-}
+  dashboard(){
+    this.route.navigate(["admin-page"])
+  }
+  program(){
+    this.route.navigate(["program"])
+  }
+  manage_user(){
+    this.route.navigate(["manage-account"])
+  }
+  partners_management(){
+    this.route.navigate(["partnership"])
+  }
 }
